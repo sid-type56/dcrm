899a358b612fd8a07333393a9c9e0d520a72fdf3
sid-type56 <sidharthholms@gmail.com>
2024-02-29 21:38:27 +0530

    feat: Mailing module

--- dcrm/settings.py
+++ dcrm/settings.py
@@ -13,6 +13,12 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 from pathlib import Path
 import os
 
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -76,14 +82,13 @@ WSGI_APPLICATION = 'dcrm.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        # 'NAME': BASE_DIR / 'db.sqlite3',
-        'NAME':'customermanagement', #database name
-        'USER':'root',
-        'PASSWORD':'root@2024',
-        'HOST':'localhost',
-        'PORT':'3306'
+    "default": {
+        "ENGINE": os.getenv("ENGINE"),
+        "NAME": os.getenv("NAME"),
+        "USER": os.getenv("USER"),
+        "PASSWORD": os.getenv("PASSWORD"),
+        "HOST": os.getenv("HOST"),
+        "PORT": os.getenv("PORT"),
     }
 }
 
---
+++ mail_module/mailing.py
@@ -0,0 +1,34 @@
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+def send_email(sender_email, recipient_email, subject, body):
+    smtp_host = os.getenv("MAIL_SERVER")   # Replace with your SMTP server host
+    smtp_port = os.getenv("MAIL_PORT")   # Replace with your SMTP server port
+    smtp_username = os.getenv("MAIL_USERNAME")   # Replace with your SMTP username
+    smtp_password = os.getenv("MAIL_PASSWORD")   # Replace with your SMTP password
+
+    # Create a MIME message
+    message = MIMEMultipart()
+    message['From'] = sender_email
+    message['To'] = recipient_email
+    message['Subject'] = subject
+
+    # Attach the email body
+    message.attach(MIMEText(body, 'plain'))
+
+    # Connect to the SMTP server and send the email
+    with smtplib.SMTP(smtp_host, smtp_port) as server:
+        server.starttls()  # Enable encryption
+        server.login(smtp_username, smtp_password)
+        server.sendmail(sender_email, recipient_email, message.as_string())
+
+    print("Email sent successfully!")
+
+# Example usage
+print("here we go")
+send_email(os.getenv("MAIL_SERVER"), os.getenv("MAIL_RECIPIENT"), 'Test Email', 'This is a test email.')
